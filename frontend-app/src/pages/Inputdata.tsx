3d80d7ea46f759c6b04a77408909c36bceb85e2b
Soonyub Hwang <nowonbun@naver.com>
2022-04-03 23:21:19 +0900

    the input was updated.

--- frontend-app/src/pages/Inputdata.tsx
+++ frontend-app/src/pages/Inputdata.tsx
@@ -1,8 +1,55 @@
+import { title } from "process";
 import { memo } from "react";
+import { SymbolDisplayPartKind } from "typescript";
+import styleSheets from "./Inputdata.module.scss";
 const Inputdata = memo(() => {
     return (
         <div className="container-fluid">
-            input
+            <div className={styleSheets.card}>
+                <div className={styleSheets.title} >
+                    Receipt
+                </div>
+                <div>
+                    <button>Payment</button>
+                    <button>Income</button>
+                </div>
+                <div>
+                    <input type="text"></input>
+                </div>
+                <div>
+                    <input type="number"></input>
+                </div>
+                <div>
+                    <div>
+                        <button>7</button>
+                        <button>8</button>
+                        <button>9</button>
+                        <button>/</button>
+                    </div>
+                    <div>
+                        <button>4</button>
+                        <button>5</button>
+                        <button>6</button>
+                        <button>*</button>
+                    </div>
+                    <div>
+                        <button>1</button>
+                        <button>2</button>
+                        <button>3</button>
+                        <button>-</button>
+                    </div>
+                    <div>
+                        <button>000</button>
+                        <button>0</button>
+                        <button>.</button>
+                        <button>+</button>
+                    </div>
+                    <div>
+                        <button>Apply</button>
+                        <button>←</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 });
--- frontend-app/src/pages/Layout.tsx
+++ frontend-app/src/pages/Layout.tsx
@@ -43,6 +43,9 @@ const Layout = memo(() => {
             <div id="content-wrapper" className="d-flex flex-column">
                 <div id="content">
                     <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                        <button id="sidebarToggleTop" className="btn btn-link d-md-none rounded-circle mr-3">
+                            <i className="fa fa-bars"></i>
+                        </button>
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item dropdown no-arrow">
                                 <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
